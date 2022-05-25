05d8f048d4452e29dd35316e36b461abe84a73d8
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-05-24 21:15:46 -0300

    �

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,23 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+import { signIn, signOut ,useSession } from 'next-auth/react'
+
+export function SignInButton() {
+
+  const { data: session } = useSession()
+  console.log(session)
+
+  return session ? (
+    <button onClick={() => signOut()} className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+        {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
+      <FaGithub color="#eba417" />
+      Sign In with Github
+    </button>
+  )
+}
