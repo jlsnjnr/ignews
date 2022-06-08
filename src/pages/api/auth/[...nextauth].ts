123c1d3a9f9c9faf0d6b885efae19d4ed00ace45
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-06-08 07:16:22 -0300

    error stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna' 
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -12,4 +17,46 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+    
+      try {
+        await fauna.query(
+          q.Let(
+            {
+              userExists: q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            },
+            q.If(
+              q.Var('userExists'),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {
+                  data: { email }
+                }
+              )
+            )
+          )
+        )
+
+        return true
+
+      } catch {
+
+        return false
+      }
+
+    },
+  }
 })
