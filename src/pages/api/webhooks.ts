420cc714ead64da0ed5419bb909741f9cff322bc
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-06-17 12:56:23 -0300

    <fix>[terminando stripe]:<finish payments>

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -25,7 +25,9 @@ export const config = {
 }
 
 const relavantsEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ])
 
 export default async function getwebhooks (req: NextApiRequest, res: NextApiResponse) {
@@ -47,13 +49,26 @@ export default async function getwebhooks (req: NextApiRequest, res: NextApiResp
       // fazer algo 
       try {
         switch (type) {
-          case 'checkout.session.completed':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
 
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription (
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+
+            break;
+
+          case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
+              true
             )
 
             break;
